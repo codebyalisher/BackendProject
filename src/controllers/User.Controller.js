d234b5f304f42f6026b9740942c21fa771ec118d
alisher <alisher.201903953@gcuf.edu.pk>
2024-08-23 07:42:00 +0500

    changes added

--- src/controllers/User.Controller.js
+++ src/controllers/User.Controller.js
@@ -1,29 +1,31 @@
-import asyncHandler  from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import {User} from "../models/User.models.js"
+import { User } from "../models/User.models.js";
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, username, password } = req.body;
   if (
     // throw new ApiError(400,"Full Name is required") or
     [fullname, email, username, password].some((field) => {
       field?.trim() === "";
-      console.log("email is :",email)
     })
   ) {
     throw new ApiError(400, "All fields are compulsory");
   }
 
-  const existedUser=User.findOne({
-    $or:[{username},{email}]
-  })
-  if(!existedUser){
-    throw new ApiError(400,"User with this email existed")
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!existedUser) {
+    throw new ApiError(400, "User with this email existed");
   }
-  const avatarLocalPath=req.files?.avatar[0]?.path;
-  const coverImageLocalPath=req.files?.coverImage[0]?.path;
-  if(avatarLocalPath){
-    throw new ApiError(400,"Avatar file is required")
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
   }
+    if (coverImageLocalPath) {
+      throw new ApiError(400, "Cover Image is required");
+    }
 });
 
 export default registerUser;
